57a6d7f208952e893307daffd9c8bc9096a485e2
qinfeng <q884822@gmail.com>
2017-10-01 20:08:51 +0800

    去掉云币api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,7 @@ class WelcomeController < ApplicationController
     @usdt_etc = Poloniex::Market.by_pair("USDT_ETC").last
     @usdt_sc = Poloniex::Market.by_pair("BTC_SC").last.to_f * Poloniex::Market.by_pair("USDT_BTC").last.to_f
     @usdt_btc = Poloniex::Market.by_pair("USDT_BTC").last
+
     @exchange_rate = Money.default_bank.get_rate('USD','CNY').to_f
     @cnydt_zec = @usdt_zec.to_f * @exchange_rate
     @cnydt_eth = @usdt_eth.to_f * @exchange_rate
@@ -14,14 +15,14 @@ class WelcomeController < ApplicationController
     @cnydt_sc = @usdt_sc.to_f * @exchange_rate
     @cnydt_btc = @usdt_btc.to_f * @exchange_rate
 
-    client_public = PeatioAPI::Client.new endpoint: 'https://yunbi.com'
-    response = client_public.get_public '/api/v2/tickers'
-    @zec_yunbi = response["zeccny"]["ticker"]["sell"]
-    @etc_yunbi = response["etccny"]["ticker"]["sell"]
-    @eth_yunbi = response["ethcny"]["ticker"]["sell"]
-    @sc_yunbi = response["sccny"]["ticker"]["sell"]
-    @btc_yunbi = response["btccny"]["ticker"]["sell"]
-    @eos_yunbi = response["eoscny"]["ticker"]["sell"]
+    # client_public = PeatioAPI::Client.new endpoint: 'https://yunbi.com'
+    # response = client_public.get_public '/api/v2/tickers'
+    # @zec_yunbi = response["zeccny"]["ticker"]["sell"]
+    # @etc_yunbi = response["etccny"]["ticker"]["sell"]
+    # @eth_yunbi = response["ethcny"]["ticker"]["sell"]
+    # @sc_yunbi = response["sccny"]["ticker"]["sell"]
+    # @btc_yunbi = response["btccny"]["ticker"]["sell"]
+    # @eos_yunbi = response["eoscny"]["ticker"]["sell"]
 
   end
 end
